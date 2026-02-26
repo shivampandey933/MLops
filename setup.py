159e35c472070b24a8e5ac19e360c2a2c02b3fb0
Shivam Pandey <pandeyshivam8765@gmail.com>
2026-02-26 13:44:14 +0530

    error and logging is done

--- setup.py
+++ setup.py
@@ -1,20 +1,25 @@
+
 from setuptools import setup, find_packages
 from typing import List
-HYPHEN_E_DOT= "-e ."
-def get_requirements(file_path:str)->List[str]:
-    requirements=[]
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+    requirements: List[str] = []
     with open(file_path) as file:
-        requirements= file.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
+        requirements = file.readlines()
+        requirements = [req.strip() for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
     return requirements
 
+
 setup(
-    name="ml-ops-project",
-    version="0.1.0",
-    author="Shivam Pandey",
-    description="A machine learning operations project",
-    packages=find_packages(),
-    install_requires=get_requirements("requirements.txt")
+   name="My mlops project",
+   version= "0.1.0",
+   author= "Shivam Pandey",
+   description="end to end mlops project using the deployment of ml models",
+   packages= find_packages(),
+   install_requires= get_requirements("requirements.txt")
+   
 )
